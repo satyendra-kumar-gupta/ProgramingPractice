c974d716982c2b585837768755bd7c64a9baf435
Satyendra Gupta <satyendra.connect@gmail.com>
2017-07-25 02:16:51 +0530

    Adding Test

---
+++ src/arthmatics/SumTest.java
@@ -0,0 +1,19 @@
+package arthmatics;
+
+/**
+ * Created by SATYENDRA_GUPTA on 25-07-2017.
+ */
+public class SumTest {
+    public static void main(String[] args) {
+        int a=5, b=7;
+
+//        int result = sum(a, b);
+//        System.out.println(result);
+
+        System.out.println(sum(a, b));
+
+    }
+    static  int sum(int x, int y){
+        return (x+y);
+    }
+}
